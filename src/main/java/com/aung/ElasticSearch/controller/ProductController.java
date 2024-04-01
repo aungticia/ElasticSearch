fba6031fbe84d60f874cf45a54ae431e2984ad0a
Aung <104800264+aungoo3@users.noreply.github.com>
2024-04-01 06:06:07 -0400

    commit

--- src/main/java/com/aung/ElasticSearch/controller/ProductController.java
+++ src/main/java/com/aung/ElasticSearch/controller/ProductController.java
@@ -1,28 +1,48 @@
 package com.aung.ElasticSearch.controller;
 
-
-import com.aung.ElasticSearch.entity.Product;
-import com.aung.ElasticSearch.service.ProductService;
+import com.aung.ElasticSearch.model.Product;
+import com.aung.ElasticSearch.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.List;
+
+
 @RestController
-@RequestMapping("/apis")
 public class ProductController {
 
     @Autowired
-    private ProductService productService;
+    private ProductRepo repo;
 
-    @GetMapping("/findAll")
-    Iterable<Product> findAll() {
-        return productService.getProducts();
+    // Get All Products
+    @GetMapping("/")
+    public ResponseEntity<Object> searchAllProducts() throws IOException {
+        List<Product> products = repo.searchAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
-    @PostMapping("/insert")
-    public Product insertProduct(@RequestBody Product product) {
-        return productService.insertProduct(product);
+    // Create or Update all Products
+    @PostMapping("/createOrUpdateProduct")
+    public ResponseEntity<Object> createOrUpdateProduct(@RequestBody Product product) throws IOException {
+        String response = repo.createOrUpdateProduct(product);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    // Get Products by Id
+    @GetMapping("/getProduct")
+    public ResponseEntity<Object> getProductById(@RequestParam String productId) throws IOException {
+        Product product =  repo.getProductById(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
 
+    // Delete Product
+    @DeleteMapping("/deleteProduct")
+    public ResponseEntity<Object> deleteProductById(@RequestParam String productId) throws IOException {
+        String response =  repo.deleteProductById(productId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 }
